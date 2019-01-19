727e4fad2cded461876224409181de0d6bc243e7
MaciekM <mjmm260@gmail.com>
2019-01-19 13:37:03 +0100

    Add "blank" project structure.

---
+++ src/main/java/com/warehouse_project/WarehouseProjectApplication.java
@@ -0,0 +1,14 @@
+package com.warehouse_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseProjectApplication.class, args);
+    }
+
+}
+
