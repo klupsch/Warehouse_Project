dcecf458acf204c11ed172d1b7dfa1fdd3727b2a
MaciekM <mjmm260@gmail.com>
2019-01-19 14:40:48 +0100

    Create body of entity class - fields, constructors and getters and setters

--- src/main/java/com/warehouse_project/warehouse/model/entity/ProductAcceptance.java
+++ src/main/java/com/warehouse_project/warehouse/model/entity/ProductAcceptance.java
@@ -1,4 +1,80 @@
 package com.warehouse_project.warehouse.model.entity;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
 public class ProductAcceptance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 35)
+    private String invoice;
+    private ProductDictionary productDictionary;
+    private Date date;
+    @Column(scale = 13, precision = 4)
+    private BigDecimal count;
+    @Column(scale = 18, precision = 2)
+    private BigDecimal price;
+
+    public ProductAcceptance() {
+    }
+
+    public ProductAcceptance(String invoice, ProductDictionary productDictionary, Date date, BigDecimal count, BigDecimal price) {
+        this.invoice = invoice;
+        this.productDictionary = productDictionary;
+        this.date = date;
+        this.count = count;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(String invoice) {
+        this.invoice = invoice;
+    }
+
+    public ProductDictionary getProductDictionary() {
+        return productDictionary;
+    }
+
+    public void setProductDictionary(ProductDictionary productDictionary) {
+        this.productDictionary = productDictionary;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getCount() {
+        return count;
+    }
+
+    public void setCount(BigDecimal count) {
+        this.count = count;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/warehouse_project/warehouse/model/entity/ProductDictionary.java
+++ src/main/java/com/warehouse_project/warehouse/model/entity/ProductDictionary.java
@@ -1,4 +1,72 @@
 package com.warehouse_project.warehouse.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ProductDictionary {
+
+    @Id
+    private Long ean;
+    @Column(length = 15)
+    private String code;
+    @Column(length = 50)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(length = 10)
+    private String unit;
+
+    public ProductDictionary() {
+    }
+
+    public ProductDictionary(Long ean, String code, String name, String description, String unit) {
+        this.ean = ean;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.unit = unit;
+    }
+
+
+    public Long getEan() {
+        return ean;
+    }
+
+    public void setEan(Long ean) {
+        this.ean = ean;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
